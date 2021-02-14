ba7b5efd4f690d54c3bb9d75c0dae2daabb77db1
Tehseen Jawed <51497155+TehseenJawed@users.noreply.github.com>
2021-02-14 18:55:07 +0500

    Created Boilerplate for React SSR

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Start project on SSR
+
+To run this project on SSR type [yarn ssr]]() on project command prompt.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [state, setState] = useState(0)
+  const Increament = () => {
+    setState(state+1)
+  }
+  const Decrement = () => {
+    setState(state-1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state}</h1>
+      <button onClick={Increament}>Increament</button>
+      <button onClick={Decrement}>Decrement</button>
     </div>
   );
 }
